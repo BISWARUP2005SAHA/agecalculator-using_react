c367f1731b1b6acaeed60a79b8f0a45f6420d645
BISWARUP2005SAHA <164617214+BISWARUP2005SAHA@users.noreply.github.com>
2024-08-25 23:48:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AgeCalculator from './components/Agecalculator';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Age calculator</h1>
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
